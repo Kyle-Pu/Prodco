70ce7a9e4d8957f89fcd2d853312922428ea5926
Kyle Pu <kylez.pu@gmail.com>
2022-03-16 16:14:05 -0700

    Setup and plans

---
+++ Producer_Consumer.c
@@ -0,0 +1,13 @@
+#include <pthread.h>
+
+int main(){
+
+  // Dynamically allocate bounded buffer of size specified by user through command line
+
+  // Create an instance of a semaphore struct for tracking open slots
+  // Create an instance of a semaphore struct for tracking filled slots
+  // Create functions for producing and consuming. Use global pointer to track index
+  
+  // Free memory
+  
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Producer Consumer
+
+A solution to the producer-consumer (bounded buffer) problem using semaphores in C.
+
+## Building
+
+Clone this repository and run `make` in the repo's directory.
+
+## Plans
+
+1) Random simulation and visualization at first
+2) Let user choose when to produce and when to consume, as well as how many items to produce/consume.
