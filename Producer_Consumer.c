57dca00e4a29913d8db34f0f65d68ab349e05ff9
Kyle Pu <kylez.pu@gmail.com>
2022-03-16 22:57:28 -0700

    Threads and thread functions beginning to be created.
    Not yet tested.

--- Producer_Consumer.c
+++ Producer_Consumer.c
@@ -2,6 +2,25 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <semaphore.h>
+
+struct params{
+  int *buffer;
+  pthread_mutex_t mutex;
+  int currPos; // Position of first unfilled slot in the buffer
+}
+  
+void produce(void *args){
+
+  struct params *buffArgs = (struct params *) args; // Cast args into pointer type
+
+}
+
+void consume(void *args){
+
+  struct params *buffArgs = (struct params *) args; // Cast args into pointer type
+  
+}
 
 int main(int argc, char **argv){
 
@@ -13,13 +32,37 @@ int main(int argc, char **argv){
 
   // Dynamically allocate bounded buffer of size specified by user through command line argument 1
   int buffSize = (int) atoi(argv[1]); // Truncates floating points
-  int* buff = (int*)calloc(buffSize, sizeof(int));   
-  // Create an instance of a semaphore struct for tracking open slots
+  int *buff = (int *)calloc(buffSize, sizeof(int));
+
+  struct params *args = malloc(sizeof(struct params));
+  args->buffer = buff;
+  args->currPos = 0;
   
+  // Semaphores to track how many slots are open and how many are filled
+  sem_t num_open;
+  sem_t num_filled;
+
+  sem_init(&num_open, 0, buffSize);
+  sem_init(&num_filled, 0, 0);
   
-  // Create an instance of a semaphore struct for tracking filled slots
-  // Create functions for producing and consuming. Use global pointer to track index
+  // Create threads
+  const int NUM_THREADS = 10;
+  pthread_t tid[NUM_THREADS];
+
+  for(int k = 0; k < NUM_THREADS; k++){
+    if(k % 2 == 0){
+      pthread_create(tid+k, NULL, produce, args);
+    }else{
+      pthread_create(tid+k, NULL, consume, args);
+    }
+  }
   
-  // Free memory
+  // Wait for threads to terminate
+  for(int k = 0; k< NUM_THREADS; k++){
+    pthread_join(tid[k], NULL);
+  }
+  
+  // Free dynamically allocated memory
+  free(buff);
   
 }
