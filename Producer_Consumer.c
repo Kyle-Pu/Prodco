48eb0da2ea159e94301606d2b05dee41a910eddf
Kyle Pu <kylez.pu@gmail.com>
2022-03-16 23:14:39 -0700

    The basic logic is done, but not tested.
    Compiles without errors, but with incompatible function pointer types warnings.

--- Producer_Consumer.c
+++ Producer_Consumer.c
@@ -6,19 +6,54 @@
 
 struct params{
   int *buffer;
-  pthread_mutex_t mutex;
+  int maxSize; // Limit of buffer
   int currPos; // Position of first unfilled slot in the buffer
-}
+
+  // For mutual exclusion
+  pthread_mutex_t mutex;
+  
+  // Semaphores to track how many slots are open and how many are filled
+  sem_t num_open;
+  sem_t num_filled;
+
+};
   
 void produce(void *args){
 
   struct params *buffArgs = (struct params *) args; // Cast args into pointer type
 
+  // Ensure proper ordering
+  // If no slots are open, this thread should wait
+  sem_wait(&buffArgs->num_open);
+
+  // Enter critical section once appropriate
+  pthread_mutex_lock(&buffArgs->mutex);
+  buffArgs->buffer[buffArgs->currPos] = 22; // https://www.youtube.com/watch?v=AgFeZr5ptV8
+  buffArgs->currPos++;
+  pthread_mutex_unlock(&buffArgs->mutex); // Leave critical section
+
+  // Wake any waiting consumers
+  sem_post(&buffArgs->num_filled);
+  
 }
 
 void consume(void *args){
 
   struct params *buffArgs = (struct params *) args; // Cast args into pointer type
+
+  // Wait until there are items to consume
+  sem_wait(&buffArgs->num_filled);
+
+  // Critical section
+  pthread_mutex_lock(&buffArgs->mutex);
+  int val = buffArgs->buffer[buffArgs->currPos];
+  buffArgs->currPos--;
+  pthread_mutex_unlock(&buffArgs->mutex);
+
+  // Wake any waiting producers
+  sem_post(&buffArgs->num_open);
+  
+  printf("Read value is %d", val);
   
 }
 
@@ -37,13 +72,9 @@ int main(int argc, char **argv){
   struct params *args = malloc(sizeof(struct params));
   args->buffer = buff;
   args->currPos = 0;
-  
-  // Semaphores to track how many slots are open and how many are filled
-  sem_t num_open;
-  sem_t num_filled;
-
-  sem_init(&num_open, 0, buffSize);
-  sem_init(&num_filled, 0, 0);
+  args->maxSize = buffSize;
+  sem_init(&args->num_open, 0, buffSize); // Buffer begins completely empty
+  sem_init(&args->num_filled, 0, 0); // Buffer begins with no items to consume
   
   // Create threads
   const int NUM_THREADS = 10;
