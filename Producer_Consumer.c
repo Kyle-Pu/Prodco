99d79df89f48c483549417cc29f59d56062f376a
Kyle Pu <kylez.pu@gmail.com>
2022-03-16 21:14:28 -0700

    User input and value checks done

--- Producer_Consumer.c
+++ Producer_Consumer.c
@@ -1,10 +1,22 @@
 #include <pthread.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int main(int argc, char **argv){
 
-  // Dynamically allocate bounded buffer of size specified by user through command line
+  // Check inputs
+  if(argc != 2 || atoi(argv[1]) <= 0){
+    errno = EINVAL;
+    perror("Invalid arguments! Please provide a positive, integer argument");
+  }
 
+  // Dynamically allocate bounded buffer of size specified by user through command line argument 1
+  int buffSize = (int) atoi(argv[1]); // Truncates floating points
+  int* buff = (int*)calloc(buffSize, sizeof(int));   
   // Create an instance of a semaphore struct for tracking open slots
+  
+  
   // Create an instance of a semaphore struct for tracking filled slots
   // Create functions for producing and consuming. Use global pointer to track index
   
